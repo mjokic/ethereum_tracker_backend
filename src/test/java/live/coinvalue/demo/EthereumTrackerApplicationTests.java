ac0a061a686743d13fb15a7201a5efd107275881
Marko Jokic <mjokic@users.noreply.github.com>
2017-08-23 11:59:56 +0200

    We've started!

---
+++ src/test/java/live/coinvalue/demo/EthereumTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package live.coinvalue.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EthereumTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
